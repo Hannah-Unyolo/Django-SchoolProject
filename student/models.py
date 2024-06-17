a1c5a5c3262ac5b8ba18123553c297210cf45227
Hannah-Unyolo <unyolohannah@gmail.com>
2024-06-17 22:26:54 +0300

    added models

---
+++ student/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    code = models.PositiveBigIntegerField()
+    date_of_birth = models.DateField()
+    country = models.CharField(max_length=20)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+# Create your models here.
