34e43bfd29ac70b60b7d402bd8d1dc9a50a55cc0
Hannah-Unyolo <unyolohannah@gmail.com>
2024-07-17 13:25:00 +0300

    corrected the urls

--- api/urls.py
+++ api/urls.py
@@ -7,25 +7,10 @@ from .views import ClassperiodListView
 
 urlpatterns=[
     path("students/", StudentListView.as_view(),name= 'student_list_view'),
-
-]
-
-urlpatterns=[
     path("teachers/", TeacherListView.as_view(),name= 'teacher_list_view'),
-]
-
-
-urlpatterns=[
     path("classes/", ClassListView.as_view(),name='class_list_view'),
-]
-
-
-urlpatterns=[
     path("classperiods/",ClassperiodListView.as_view(), name='classperiod_list_view'),
-]
-
-
-urlpatterns=[
     path("courses/", CourseListView.as_view(), name= 'course_list_view'),
 ]
+
 
